a5fa1801e3f404f1bc63fe59a95a1c30b19dfed3
Indrawan <indrawanismail0@gmail.com>
2024-12-25 11:36:01 +0800

    update

--- src/components/Utilities/Pagination.jsx
+++ src/components/Utilities/Pagination.jsx
@@ -1,17 +1,26 @@
 const Pagination = ({ page, lastPage, setPage }) => {
 
+
+    const scrollTop = () => {
+        scrollTo({
+            behavior: "smooth",
+            top: 0
+        })
+    }
     const handleNextPage = () => {
         setPage((prevState) => prevState + 1)
+        scrollTop()
     }
     const handlePrevPage = () => {
         setPage((prevState) => prevState - 1)
+        scrollTop()
     }
 
     return (
         <div className="flex justify-center items-center py-4 px-2 gap-4 text-color-primary text-2xl">
             <button onClick={handlePrevPage} className="transition-all hover:text-color-accent">Prev</button>
-            <p>{page} 0f {lastPage}</p>
-
+            <p className="gap-4"> {page} 0f 1000{lastPage}</p>
+{/* di atas di api jikanya ga ada, bagian last page jadi manual */}
             <button onClick= {handleNextPage} className="transition-all hover:text-color-accent">Next</button>
         </div>
     )

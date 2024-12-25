0adb1321b4fd7130c189e9198ded18863c62e8af
Indrawan <indrawanismail0@gmail.com>
2024-12-25 20:04:59 +0800

    added feature

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,12 @@
 import Header from "@/components/CardAnimeList/header"
 import CardAnimeList from "@/components/CardAnimeList"
+import { getAnimeResponse } from "./libs/api-libs"
 
-const Home = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`)
-  const topAnime = await response.json()
+
+const Page = async () => {
+  const topAnime = await getAnimeResponse("top/anime", "limit=8")
+
+
   
   return (
     <>
@@ -16,4 +19,4 @@ const Home = async () => {
 )
 }
 
-export default Home
+export default Page
--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,12 +1,13 @@
+import { getAnimeResponse } from "@/app/libs/api-libs"
 import CardAnimeList from "@/components/CardAnimeList"
 import Header from "@/components/CardAnimeList/header"
 
 
 const Page = async ( { params } ) => {
     const {keyword} = params
+
     const decodedKeyword = decodeURI(keyword)
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodedKeyword}`)
-    const searchAnime = await response.json()
+    const searchAnime = await getAnimeResponse("anime", `q=${decodedKeyword}`)
 
 
     return (

c9cdbac9d885df7f400d6bc5c418f67ca3f08647
Indrawan <indrawanismail0@gmail.com>
2024-12-27 22:07:25 +0800

    updt

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -3,14 +3,14 @@ import Image from "next/image"
 
 const Page = async ({ params: { id } }) => {
     const anime = await getAnimeResponse(`anime/${id}`)
-console.log(anime)
+
     return (
         <>
             <div className="pt-4 px-4">
-                <h3 className="text-2xl text-color-primary font-bold">{anime.data.title} - {anime.data.episodes}
+                <h3 className="text-2xl text-color-primary">{anime.data.title} - {anime.data.episodes}
                 </h3>
             </div>
-            <div className="pt-4 px-4 gap-2 flex sm:flex-nowrap flex-wrap text-color-primary justify-center items-center">
+            <div className="pt-4 px-4 flex gap-2 text-color-primary overflow-x-auto">
                 <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2">
                     <h3>Ranking :</h3>
                     <p>{anime.data.rank}</p>
@@ -24,7 +24,7 @@ console.log(anime)
                     <p>{anime.data.members}</p>
                 </div>
                 <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2">
-                    <h3>Popularitas :</h3>
+                    <h3>populer :</h3>
                     <p>{anime.data.popularity}</p>
                 </div>
                 <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2">
@@ -35,17 +35,17 @@ console.log(anime)
                     <h3>Episode :</h3>
                     <p>{anime.data.episodes}</p>
                 </div>
-            </div>
-            <div className="flex sm:flex-nowrap flex-wrap pt-4 px-4 gap-2 text-color-primary">
-                <Image
-                    src={anime.data.images.webp.image_url}
-                    alt={anime.data.images.jpg.image_url}
-                    width={250}
-                    height={250}
-                    className="w-full rounded object-cover"
-                />
+                    </div>
+                <div className="flex sm:flex-nowrap flex-wrap pt-4 px-4 gap-2 text-color-primary">
+                    <Image
+                        src={anime.data.images.webp.image_url}
+                        alt={anime.data.images.jpg.image_url}
+                        width={250}
+                        height={250}
+                        className="w-full rounded object-cover"
+                    />
                 <p className="text-justify text-xl">{anime.data.synopsis}</p>
-            </div>
+                </div>
         </>
     )
 }

26edb272aee01ac66f4928fcdf9681aebec47d7a
Indrawan <indrawanismail0@gmail.com>
2024-12-28 21:47:46 +0800

    add video player from yt

--- src/components/Utilities/VideoPlayer.jsx
+++ src/components/Utilities/VideoPlayer.jsx
@@ -1,21 +1,48 @@
 "use client"
+
+import { useState } from "react"
 import YouTube from "react-youtube"
 
 const VideoPlayer = ({ youtubeId }) => {
+    const [isOpen, setIsOpen] = useState(true)
+
+    const handleVideoPlayer = () => {
+        setIsOpen((prevState) => !prevState)
+    }
+
     const option = {
         width: "300",
-        height:"250"
+        height: "250"
     }
-    return (
-        <div className="fixed bottom-2 right-2">
-            <button className="text-color-primary float-right mr-1 mb-1 bg-color-secondary px-3">
-                X
-            </button>
-            <YouTube VideoId={youtubeId} 
-            onReady={(event) => event.target.pauseVideo()}
-            opts={option}/>
-        </div>
-    )
+
+    const Player = () => {
+        return (
+            <div className="fixed bottom-2 right-2">
+                <button
+                    onClick={handleVideoPlayer}
+                    className="text-color-primary float-right mr-1 mb-1 bg-color-secondary px-3">
+                    X
+                </button>
+                <YouTube VideoId={youtubeId}
+                    onReady={(event) => event.target.pauseVideo()}
+                    opts={option} 
+                    onError={() => alert("Video is not avaible for now, please try later")}
+                    />
+                    
+            </div>
+        )
+    }
+
+    const ButtonOpenPlayer = () => {
+        return (
+    <button onClick={handleVideoPlayer}
+    className="fixed bottom-5 text-xl font-bold p-2 rounded-xl hover:bg-color-accent transition-all shadow-xl right-5 w-32 bg-color-primary text-color-dark">
+        Tonton Trailer
+        </button>
+        )
+    }
+
+    return isOpen ? <Player/> : <ButtonOpenPlayer/>
 }
 
 export default VideoPlayer
